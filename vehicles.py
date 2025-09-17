d758a900e101c0288b63848a8f0a397c7149902f
Gitusgithub <gitujohn9@gmail.com>
2025-09-17 10:22:22 +0300

    Add editions and reliabilty column to vehicles

--- vehicles.py
+++ vehicles.py
@@ -1,8 +1,17 @@
 # vehicles.py
 
-vehicles = ["Toyota", "Honda", "Ford", "Mazda", "BMW"]
+# Each vehicle will be stored as a dictionary
+vehicles = [
+    {"name": "Toyota", "edition": "2020", "reliability": 9},
+    {"name": "Honda", "edition": "2019", "reliability": 8},
+    {"name": "Ford", "edition": "2021", "reliability": 7},
+    {"name": "Mazda", "edition": "2018", "reliability": 8},
+    {"name": "BMW", "edition": "2022", "reliability": 6},
+]
 
-print("List of Vehicles:")
+print("Vehicle List with Editions and Reliability:")
+print("Name      Edition   Reliability")
+print("--------------------------------")
+
 for v in vehicles:
-    print("-", v)
-    
+    print(f"{v['name']:<10} {v['edition']:<8} {v['reliability']}")
